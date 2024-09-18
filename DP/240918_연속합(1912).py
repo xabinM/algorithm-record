c5af894cd0cba2e5af98bb0ec2538d65c9c0167f
Min <binine2@gmail.com>
2024-09-18 23:50:58 +0900

    dp

--- DP/240918_연속합(1912).py
+++ DP/240918_연속합(1912).py
@@ -1,6 +1,13 @@
 n = int(input())
 nums = list(map(int, input().split()))
-max = 0
 
 def solution(n, nums):
     dp = [0] * n
+    dp[0] = nums[0]
+
+    for i in range(1, n):
+        dp[i] = max(nums[i], nums[i] + dp[i - 1])
+
+    return max(dp)
+
+print(solution(n, nums))
