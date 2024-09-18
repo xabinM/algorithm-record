eb67ced6250192d9a00741b9b017d5db3cc333b9
Min <binine2@gmail.com>
2024-09-18 23:55:03 +0900

    dp

--- DP/240918_연속합(1912).py
+++ DP/240918_연속합(1912).py
@@ -2,11 +2,11 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 def solution(n, nums):
-    dp = [0] * n
+    dp = [0] * n    # dp[i] 는 i번째 요소까지의 최대합을 기록
     dp[0] = nums[0]
 
     for i in range(1, n):
-        dp[i] = max(nums[i], nums[i] + dp[i - 1])
+        dp[i] = max(nums[i], nums[i] + dp[i - 1]) # nums[i] 와 dp[i - 1]에 이어서 더한 값 중 더 큰 값을 선택
 
     return max(dp)
 
