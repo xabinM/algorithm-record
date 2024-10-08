b5b16d4886d933f95c7ce9d0b03f482bf6cb036f
Min <binine2@gmail.com>
2024-10-08 18:49:28 +0900

    BFS&DFS

--- DFS&BFS/241008_A와B(12904).py
+++ DFS&BFS/241008_A와B(12904).py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def solution(S, T):
 
     while len(T) > len(S):
@@ -12,7 +14,32 @@ def solution(S, T):
     else:
         print(0)
 
+def bfs(S, T):
+    q = deque([T])
+    visited = set([T])
+
+    while q:
+        cur = q.popleft()
+
+        if cur == S:
+            return print(1)
+
+        if len(cur) > len(S):
+            if cur[-1] == "A":
+                cur = cur[:-1]
+                if cur not in visited:
+                    visited.add(cur)
+                    q.append(cur)
+            else:
+                cur = ''.join(reversed(cur[:-1]))
+                if cur not in visited:
+                    visited.add(cur)
+                    q.append(cur)            
+
+    return print(0)
+
 S = input()
 T = input()
 
 solution(S, T)
+bfs(S, T)
