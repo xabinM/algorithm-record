2640733d4013bd03c6c0ed2fd9432869ab466a1d
xabin <binine2@gmail.com>
2025-01-10 11:38:17 +0900

    algo: 장훈이 선반 백트레킹 시간초과

--- ssafy/JangHoonShelf.py
+++ ssafy/JangHoonShelf.py
@@ -1,11 +1,32 @@
+def backtrace(limit, cur_sum):
+    global result
+
+    if cur_sum >= result:
+        return
+
+    if limit <= cur_sum:
+        result = min(result, cur_sum)
+        print(result)
+        return
+
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            backtrace(limit, cur_sum + S[i])
+            visited[i] = False
+
 T = int(input())
 
 for t in range(1, T + 1):
     N, B = map(int, input().split())
-
     S = list(map(int, input().split()))
 
-    min_high = 0
+    S.sort(reverse=True)
 
-    for i in range(len(S)):
+    visited = [False] * N
+    result = float('inf')
+
+    backtrace(B, 0)
+
+    print("#{}".format(t), result - B)
 
